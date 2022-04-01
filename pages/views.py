a17749a55ff8e479ee204e95e81d0422c5ab7eca
Abdechakour <abdechakourmechri@gmail.com>
2022-04-01 14:35:12 +0000

    Featured Cars, Latest Cars & Image Gallery

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from . import models
-
+from .models import Team
+from cars.models import Car
 # Create your views here.
 def home(request):
-    teams = models.Team.objects.all()
-    context = {"teams" : teams}
+    teams = Team.objects.all()
+    featured_cars = Car.objects.order_by("-created_date").filter(is_featured=True)
+    all_cars = Car.objects.order_by("-created_date")
+    context = {
+        "teams" : teams,
+        "featured_cars" : featured_cars,
+        "all_cars" : all_cars,
+    }
     return render(request, 'pages/home.html', context=context, )
 
 def about(request):
-    teams = models.Team.objects.all()
+    teams = Team.objects.all()
     context = {"teams" : teams}
     return render(request, 'pages/about.html', context= context)
 
